31aeb6ced6fbe0f5eaaa1c7c3f07b9c3693824d9
zangdale <getbuguai@outlook.com>
2022-10-31 16:26:17 +0800

    ➕

--- README.md
+++ README.md
@@ -5,6 +5,20 @@
 go install github.com/zangdale/httpfile@latest
 
 使用
-只需要在想要启动该服务的文件夹中执行
-httpfile
-命令即可
+
+```
+httpfile -h
+Usage of httpfile:
+  -d string
+        http file directory
+  -p uint
+        http server port
+```
+
+```
+httpfile     
+/Users/zangdale/test/go/httpfile      [::]:56095
+http://0.0.0.0:56095
+http://192.168.3.254:56095
+http://192.168.236.86:56095
+```
--- main.go
+++ main.go
@@ -1,22 +1,72 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 )
 
-func main() {
-	d, e := os.Getwd()
-	if e != nil {
-		log.Fatal(e)
-	}
-	fmt.Println(d, "    :8080")
+var (
+	port = flag.Uint("p", 0, "http server port")
+	dir  = flag.String("d", "", "http file directory")
+)
 
-	http.Handle("/", http.FileServer(http.Dir(d)))
-	e = http.ListenAndServe(":8080", nil)
-	if e != nil {
-		log.Fatal(e)
+func main() {
+	flag.Parse()
+	portS := func() string {
+		if port == nil {
+			return ":0"
+		} else {
+			return fmt.Sprintf(":%d", *port)
+		}
+	}()
+	dirS := func() string {
+		if dir == nil || *dir == "" {
+			d, err := os.Getwd()
+			if err != nil {
+				log.Fatal(err)
+			}
+			return d
+		} else {
+			return *dir
+		}
+	}()
+
+	ln, err := net.Listen("tcp", portS)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	addr := ln.Addr().(*net.TCPAddr)
+
+	fmt.Println(dirS, "    ", addr.String())
+	showAddrs(addr.AddrPort().Port())
+
+	err = http.Serve(ln, http.FileServer(http.Dir(dirS)))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func showAddrs(port uint16) {
+	show := func(ip string) {
+		fmt.Printf("http://%s:%d\n", ip, port)
+	}
+
+	show("0.0.0.0")
+
+	a, err := net.InterfaceAddrs()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	for _, v := range a {
+		ip := v.(*net.IPNet)
+		if !ip.IP.IsGlobalUnicast() {
+			continue
+		}
+		show(ip.IP.String())
 	}
 }
